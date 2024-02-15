0507d8271f4e8ccbfa580683f33386ec85b1ecfc
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2024-02-14 22:25:58 -0500

    changed code to asyn/await statments

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Employee-Tracker
+
+## Description
+
+This project was made with inquirer, node.js, and mysql. The user will be given a list of choices and then prompted to select or input information into the database. The user can view departments, view roles, view employees, add a department, add a role, add an employee, and update an employee's role in a company. This is invoked within the CL. 
+
+## Installation
+
+N/A
+
+## Usage
+
+Click the link to view an example of the database. 
+
+
+[Employee Database Screenshot](<image/Employee Database Screenshot.png>)
+
+
+    
+Click the link for a video walkthrough demonstration of the Employee Tracker project. 
+
+[Link to Video]()
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mysql = require("mysql2");
 require("dotenv").config();
 const inquirer = require("inquirer");
 
-// Connect to the database using the details from .env
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -10,7 +9,6 @@ const db = mysql.createConnection({
   database: process.env.DB_DATABASE,
 });
 
-// Define queryDB function
 const queryDB = (sql, params = []) => {
   return new Promise((resolve, reject) => {
     db.query(sql, params, (err, results) => {
@@ -25,10 +23,9 @@ const queryDB = (sql, params = []) => {
   });
 };
 
-// Function to start the app with user input questions for the database
-function startApp() {
-  inquirer
-    .prompt([
+async function startApp() {
+  while (true) {
+    const answers = await inquirer.prompt([
       {
         type: "list",
         name: "option",
@@ -44,183 +41,169 @@ function startApp() {
           "Exit",
         ],
       },
-    ])
-    .then((answers) => {
-      // Handle user choice
-      switch (answers.option) {
-        case "View all departments":
-          // Call a function to view all departments using the queryDB function
-          queryDB("SELECT * FROM departments");
-          break;
-        case "Add a department":
-          inquirer
-            .prompt([
-              {
-                type: "input",
-                name: "departmentName",
-                message: "What is the name of the department?",
-              },
-            ])
-            .then((data) => {
-              queryDB("INSERT INTO departments (name) VALUES (?)", [
-                data.departmentName,
-              ]);
-            });
-          break;
+    ]);
 
-        case "Add a role":
-          // Fetch the list of departments from the database
-          queryDB("SELECT id, name FROM departments")
-            .then((departmentChoices) => {
-              inquirer
-                .prompt([
-                  {
-                    type: "input",
-                    name: "roleTitle",
-                    message: "What is the title of the role?",
-                  },
-                  {
-                    type: "input",
-                    name: "roleSalary",
-                    message: "What is the salary for this role?",
-                  },
-                  {
-                    type: "list",
-                    name: "roleDepartment",
-                    message: "Please choose a department for this role:",
-                    choices: departmentChoices.map((department) => ({
-                      name: department.name,
-                      value: department.id,
-                    })),
-                  },
-                ])
-                .then((data) => {
-                  queryDB(
-                    "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)",
-                    [data.roleTitle, data.roleSalary, data.roleDepartment]
-                  );
-                });
-            })
-            .catch((error) => {
-              console.error("Error during role creation:", error);
-            });
-break;
-        case "View all roles":
-          queryDB("SELECT * FROM roles");
-          break;
+    switch (answers.option) {
+      case "View all departments":
+        await queryDB("SELECT * FROM departments");
+        break;
+      case "Add a department":
+        const data = await inquirer.prompt([
+          {
+            type: "input",
+            name: "departmentName",
+            message: "What is the name of the department?",
+          },
+        ]);
+        await queryDB("INSERT INTO departments (name) VALUES (?)", [data.departmentName]);
+        break;
+      case "Add a role":
+        const departmentChoices = await queryDB("SELECT id, name FROM departments");
+        const roleData = await inquirer.prompt([
+          {
+            type: "input",
+            name: "roleTitle",
+            message: "What is the title of the role?",
+          },
+          {
+            type: "input",
+            name: "roleSalary",
+            message: "What is the salary for this role?",
+          },
+          {
+            type: "list",
+            name: "roleDepartment",
+            message: "Please choose a department for this role:",
+            choices: departmentChoices.map((department) => ({
+              name: department.name,
+              value: department.id,
+            })),
+          },
+        ]);
+        await queryDB("INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", [
+          roleData.roleTitle,
+          roleData.roleSalary,
+          roleData.roleDepartment,
+        ]);
+        break;
+      case "View all roles":
+        await queryDB("SELECT * FROM roles");
+        break;
+      case "Add an employee":
+        const roleChoices = await queryDB("SELECT id, title FROM roles");
+        const managerChoices = await queryDB("SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employees");
+        const employeeData = await inquirer.prompt([
+          {
+            type: "input",
+            name: "employeeFirstName",
+            message: "What is the employee's first name?",
+          },
+          {
+            type: "input",
+            name: "employeeLastName",
+            message: "What is the employee's last name?",
+          },
+          {
+            type: "list",
+            name: "employeeRoleId",
+            message: "Choose the employee's role:",
+            choices: roleChoices.map((role) => ({
+              name: role.title,
+              value: role.id,
+            })),
+          },
+          {
+            type: "list",
+            name: "employeeManagerId",
+            message: "Please choose the employee's manager:",
+            choices: managerChoices.map((manager) => ({
+              name: manager.name,
+              value: manager.id,
+            })),
+          },
+        ]);
+        const employeeSql = "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+        const employeeParams = [
+          employeeData.employeeFirstName,
+          employeeData.employeeLastName,
+          employeeData.employeeRoleId,
+          employeeData.employeeManagerId,
+        ];
+        await queryDB(employeeSql, employeeParams);
+        break;
+      case "View all employees":
+        const filterAnswers = await inquirer.prompt([
+          {
+            type: "list",
+            name: "filterOption",
+            message: "How would you like to filter employees?",
+            choices: ["View all employees", "Filter by manager"],
+          },
+        ]);
 
-        case "Add an employee":
-          // Fetch the list of roles from the database
-          queryDB("SELECT id, title FROM roles").then((roleChoices) => {
-            // Fetch the list of managers from the database
-            queryDB(
-              "SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employees"
-            ).then((managerChoices) => {
-              inquirer
-                .prompt([
-                  {
-                    type: "input",
-                    name: "employeeFirstName",
-                    message: "What is the employee's first name?",
-                  },
-                  {
-                    type: "input",
-                    name: "employeeLastName",
-                    message: "What is the employee's last name?",
-                  },
-                  {
-                    type: "list",
-                    name: "employeeRoleId",
-                    message: "Choose the employee's role:",
-                    choices: roleChoices.map((role) => ({
-                      name: role.title,
-                      value: role.id,
-                    })),
-                  },
-                  {
-                    type: "list",
-                    name: "employeeManagerId",
-                    message: "Please choose the employee's manager:",
-                    choices: managerChoices.map((manager) => ({
-                      name: manager.name,
-                      value: manager.id,
-                    })),
-                  },
-                ])
-                .then((data) => {
-                  const sql =
-                    "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
-                  const params = [
-                    data.employeeFirstName,
-                    data.employeeLastName,
-                    data.employeeRoleId,
-                    data.employeeManagerId,
-                  ];
-
-                  queryDB(sql, params);
-                })
-                .catch((error) => {
-                  console.error("Error during employee creation:", error);
-                });
-            });
-          });
-          break;
-
+        switch (filterAnswers.filterOption) {
           case "View all employees":
-            inquirer
-              .prompt([
-                {
-                  type: "list",
-                  name: "filterOption",
-                  message: "How would you like to filter employees?",
-                  choices: ["View all employees", "Filter by manager"],
-                },
-              ])
-              .then((filterAnswers) => {
-                switch (filterAnswers.filterOption) {
-                  case "View all employees":
-                    queryDB("SELECT * FROM employees");
-                    break;
-                  case "Filter by manager":
-                    // Fetch the list of managers from the database
-                    queryDB(
-                      "SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employees"
-                    ).then((managerChoices) => {
-                      inquirer
-                        .prompt([
-                          {
-                            type: "list",
-                            name: "managerFilter",
-                            message: "Choose the manager:",
-                            choices: managerChoices.map((manager) => ({
-                              name: manager.name,
-                              value: manager.name, 
-                            })),
-                          },
-                        ])
-                        .then((data) => {
-                          // Use the selected manager's name to filter employees
-                          queryDB("SELECT * FROM employees WHERE CONCAT(first_name, ' ', last_name) = ?", [
-                            data.managerFilter,
-                          ]);
-                        });
-                    });
-                    break;
-                }
-              });
+            await queryDB("SELECT * FROM employees");
             break;
-          case "Update an employee role": 
-      }
-    });
+          case "Filter by manager":
+            const managerChoices = await queryDB("SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employees");
+            const managerData = await inquirer.prompt([
+              {
+                type: "list",
+                name: "managerFilter",
+                message: "Choose the manager:",
+                choices: managerChoices.map((manager) => ({
+                  name: manager.name,
+                  value: manager.name,
+                })),
+              },
+            ]);
+            await queryDB("SELECT * FROM employees WHERE CONCAT(first_name, ' ', last_name) = ?", [managerData.managerFilter]);
+            break;
+        }
+        break;
+      case "Update an employee role":
+        const employeeChoices = await queryDB("SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employees");
+        const updatedEmployeeData = await inquirer.prompt([
+          {
+            type: "list",
+            name: "employeeToUpdate",
+            message: "Choose an employee you want to update:",
+            choices: employeeChoices.map((employee) => ({
+              name: employee.name,
+              value: employee.id,
+            })),
+          },
+          {
+            type: "input",
+            name: "newRole",
+            message: "What is the new role for this employee?",
+          },
+        ]);
+
+        const roleID = (await queryDB("SELECT id FROM roles WHERE title = ?", [updatedEmployeeData.newRole]))[0].id;
+
+        const updateSql = "UPDATE employees SET role_id = ? WHERE id = ?";
+        const updateParams = [roleID, updatedEmployeeData.employeeToUpdate];
+
+        await queryDB(updateSql, updateParams);
+        console.log("Employee role updated successfully!");
+        break;
+      case "Exit":
+        process.exit(0);
+        break;
+      default:
+        console.log("Invalid option. Please try again.");
+        break;
+    }
+  }
 }
 
-// Connect to the database and start the application
 db.connect((err) => {
   if (err) {
     console.error("Error connecting to MySQL:", err);
     return;
   }
   console.log("Connected to MySQL");
-  // Call the startApp function to start the application
   startApp();
 });
